0312365e5b7b0a44d9d04e4daca318528a2935ae
Jakob Lierman <jakob.lierman@gmail.com>
2018-12-21 01:14:10 +0100

    Design login and signup screens

--- Flits/StartViewController.swift
+++ Flits/StartViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  StartViewController.swift
 //  Flits
 //
 //  Created by Jakob Lierman on 20/12/2018.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class StartViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
