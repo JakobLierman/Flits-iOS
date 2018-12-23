08d2d032f66e601296f4e3b34d556a919b1c9f0c
Jakob Lierman <jakob.lierman@gmail.com>
2018-12-24 00:41:29 +0100

    Changed folder structure

---
+++ Flits/Models/SpeedCamera.swift
@@ -0,0 +1,13 @@
+//
+//  SpeedCamera.swift
+//  Flits
+//
+//  Created by Jakob Lierman on 24/12/2018.
+//  Copyright © 2018 Jakob Lierman. All rights reserved.
+//
+
+import UIKit
+
+class SpeedCamera: Codable {
+
+}
