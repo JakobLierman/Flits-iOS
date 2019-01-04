f155477ea345b647534e8d90f3312e36a6df25f6
Jakob Lierman <jakob.lierman@gmail.com>
2019-01-05 00:25:50 +0100

    Added image to PoliceCheck

--- Flits/Models/PoliceCheck.swift
+++ Flits/Models/PoliceCheck.swift
@@ -13,16 +13,18 @@ class PoliceCheck: Item {
     // MARK: Properties
     let location: String
     let description: String?
+    var imagePath: String?
     var likes: Set<String>
     var dislikes: Set<String>
     let timeCreated: Date
     let expireDate: Date
     
     // MARK: Constructors
-    init(location: String, details: String? = nil) {
+    init(location: String, description: String? = nil, imagePath: String? = nil) {
         self.timeCreated = Date()
         self.location = location
-        self.description = details
+        self.description = description
+        self.imagePath = imagePath
         self.likes = []
         self.dislikes = []
         self.expireDate = Date(timeInterval: 21600, since: timeCreated)
