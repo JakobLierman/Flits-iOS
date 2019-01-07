6c2a257382111308d52ce2dad399ffbbb50409ea
Jakob Lierman <jakob.lierman@gmail.com>
2019-01-07 10:46:21 +0100

    Added delete button to SpeedCameraDetailView

--- Flits/Controllers/SpeedCameraDetailViewController.swift
+++ Flits/Controllers/SpeedCameraDetailViewController.swift
@@ -12,10 +12,13 @@ import Firebase
 class SpeedCameraDetailViewController: UIViewController {
     
     // MARK: - Properties
+    @IBOutlet weak var deleteButton: UIBarButtonItem!
     @IBOutlet weak var locationText: UILabel!
     @IBOutlet weak var descriptionText: UILabel!
     @IBOutlet weak var kindText: UILabel!
     @IBOutlet weak var image: UIImageView!
+    // Firebase
+    let db = Firestore.firestore()
     
     var speedCamera: SpeedCamera? {
         didSet {
@@ -50,7 +53,10 @@ class SpeedCameraDetailViewController: UIViewController {
                     self.image.image = UIImage(data: data!)
                 }
             }
-            
+        }
+        // Enable delete functionality if user is creator
+        if Auth.auth().currentUser?.uid == speedCamera?.userID {
+            deleteButton.isEnabled = true
         }
     }
     
@@ -59,7 +65,30 @@ class SpeedCameraDetailViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
-
+    @IBAction func deleteAction(_ sender: Any) {
+        if Auth.auth().currentUser?.uid == speedCamera?.userID {
+            // Show confirmation alert
+            let alertController = UIAlertController(title: "Flitser verwijderen", message: "Bent u zeker?", preferredStyle: .alert)
+           
+            let cancelAction = UIAlertAction(title: "Annuleren", style: .cancel, handler: nil)
+            let deleteAction = UIAlertAction(title: "Verwijderen", style: .destructive, handler: { (UIAlertAction) in
+                // Delete item from database
+                self.db.collection("speedCameras").document(self.speedCamera!.id).delete() { err in
+                    if let err = err {
+                        print("Error removing document: \(err)")
+                    } else {
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                }
+            })
+            
+            alertController.addAction(cancelAction)
+            alertController.addAction(deleteAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
